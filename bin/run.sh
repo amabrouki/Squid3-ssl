f3c2809e4754add4c1470cafbfc97a1494c5f61e
amabrouki <mabrouki.abderrahim@gmail.com>
2016-07-20 11:07:39 +0200

    Update run.sh

--- bin/run.sh
+++ bin/run.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # Install cert
-cp -v /etc/squid3/certs/*.crt /usr/share/ca-certificates/
-find /etc/squid3/certs -name '*.crt'  -printf "%f\n" >> /etc/ca-certificates.conf
-/usr/sbin/update-ca-certificates --fresh
+#cp -v /etc/squid3/certs/*.crt /usr/share/ca-certificates/
+#find /etc/squid3/certs -name '*.crt'  -printf "%f\n" >> /etc/ca-certificates.conf
+#/usr/sbin/update-ca-certificates --fresh
 
 # Prep cache directory
 /usr/sbin/squid3 -z -N -f /etc/squid3/squid3-ssl.conf
