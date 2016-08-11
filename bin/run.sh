9e6b4ab7748cb4cdb099332216c8e019f7d770e2
root <mabrouki.abderrahim@gmail.com>
2016-08-11 09:36:42 +0200

    permission

--- bin/run.sh
+++ bin/run.sh
@@ -3,6 +3,16 @@
 cp -v /etc/squid3/certs/*.crt /usr/share/ca-certificates/
 find /etc/squid3/certs -name '*.crt'  -printf "%f\n" >> /etc/ca-certificates.conf
 /usr/sbin/update-ca-certificates --fresh
+
+chown proxy:proxy /etc/squid3/squidGuard.conf
+chown -R proxy:proxy /var/lib/squidguard/db
+chown -R proxy:proxy /var/log/squid/
+
+chmod 666 /etc/squid3/squidGuard.conf
+chmod -R 666 /var/lib/squidguard/db
+chmod -R 666 /var/log/squid/
+chmod 755 /var/log/squid
+
 # Prep cache directory
 /usr/sbin/squid3 -z -N -f /etc/squid3/squid3-ssl.conf
 # Run squid
